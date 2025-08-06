ecc633371d00c5abb8274d9df2a530e3268a404c
RiyaShukla90 <112087174+RiyaShukla90@users.noreply.github.com>
2025-08-06 10:52:01 +0530

    new changes

---
+++ force-app/main/default/lwc/home/home.js
@@ -0,0 +1,7 @@
+import { LightningElement } from 'lwc';
+
+export default class Home extends LightningElement {
+    user = 'Riya';
+    contact = '987654876';
+    messege = 'Welcome to salesforce';
+}
